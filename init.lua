e92d6e49072074b470cc3706a1305629cc5ac37a
Raul Jara <raul.c.jara@gmail.com>
2022-04-04 10:32:10 -0400

    initial port of most of what i want from old vimrc

---
+++ init.lua
@@ -0,0 +1,99 @@
+function P(obj)
+  print (vim.inspect(obj))
+end
+
+--------------------
+-- global options --
+--------------------
+
+-- status line
+-- todo: figure out status line
+-- vim.opt.statusline = '%-50(%-0f%-0m%-0r%-0h%) %(%l/%L%)'
+
+-- line numbers
+vim.opt.number = true
+vim.opt.rnu = true
+
+-- substitution
+vim.opt.gdefault = true
+
+-- search
+vim.opt.smartcase = true
+
+-- tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- tab/eol
+vim.opt.list = true
+vim.opt.listchars = ''
+vim.opt.listchars:append('eol:¬')
+vim.opt.listchars:append('tab:▸ ')
+vim.opt.listchars:append('nbsp:¤')
+P(vim.opt.listchars:get())
+
+-------------------
+-- Global remaps --
+-------------------
+
+vim.g.mapleader = ','
+
+local function map(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true })
+end
+
+local function nmap(lhs, rhs)
+  map('n', lhs, rhs)
+end
+
+local function imap(lhs, rhs)
+  map('i', lhs, rhs)
+end
+
+--escape
+imap('jj', '<Esc>')
+
+-- Insert spaces without entering insert mode
+nmap('<Space>', 'i<Space><Esc>')
+
+-- save on
+nmap('<CR>', ':w<CR>')
+nmap('Z', 'ZZ')
+
+-- split navigation
+nmap('<C-h>', '<C-w>h')
+nmap('<C-j>', '<C-w>j')
+nmap('<C-k>', '<C-w>k')
+nmap('<C-l>', '<C-w>l')
+
+-- config files for edit
+nmap('<leader>ec', ':tabedit $MYVIMRC<CR>')
+nmap('<leader>rc', ':source $MYVIMRC<CR>')
+
+
+-- relative file edits (non fzf')
+nmap('<leader>ew', ':e <C-R>=expand("%:p:h") . "/" <CR>')
+nmap('<leader>es', ':sp <C-R>=expand("%:p:h") . "/" <CR>')
+nmap('<leader>ev', ':vsp <C-R>=expand("%:p:h") . "/" <CR>')
+nmap('<leader>et', ':tabe <C-R>=expand("%:p:h") . "/" <CR>')
+nmap('<leader>ef', ':vsp <C-R><C-P><CR>')  --edit path under cursor
+
+-- when using z to recenter around the cursor, don't go all the way to the top or bottom
+nmap('zb', 'zb5<C-E>')
+nmap('zt', 'zt5<C-Y>')
+
+-- clear search
+nmap('<leader>cx', ':let @/ = ""<CR>')
+
+------------------
+-- autocommands --
+------------------
+
+-- remove trailing whitespace
+-- todo: remove vim.cmd
+vim.cmd([[autocmd BufWritePre * :%s/\s\+$//e]])
+
+-- Don't put anything after this
+print "init.lua loaded again"
