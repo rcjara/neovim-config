633d8bc5cf2281fc543c4f61a11eb3cf94bb6839
Raul Jara <raul.c.jara@gmail.com>
2022-05-11 15:43:05 -0400

    cleanup telescope_config a little

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -1,10 +1,24 @@
 local nmap = require('utils').remaps.nmap
-local telescope = require('telescope.builtin')
+local telescope = require('telescope')
+local builtin = require('telescope.builtin')
 
-nmap('ff', function() telescope.find_files() end)
-nmap('fg', function() telescope.live_grep() end)
-nmap('fb', function() telescope.buffers() end)
-nmap('fh', function() telescope.help_tags() end)
+require("telescope").setup {
+  defaults = {
+    prompt_prefix = "= ",
+    -- todo: ask someone how the hell mappings work
+    mappings = {
+      n = {
+        -- this line currently does nothing, even though I believe it should turn off a telescope default key command
+        ['<C-t>'] = false,
+      }
+    }
+  }
+}
+
+nmap('<leader>ff', builtin.find_files)
+nmap('<leader>fg', builtin.live_grep)
+nmap('<leader>fb', builtin.buffers)
+nmap('<leader>fh', builtin.help_tags)
 
 local utils = require('utils')
 utils.setup_au_reload_command()
