bde23915708c642e4d23f290d78b6548024ecfaf
Raul Jara <raul.c.jara@gmail.com>
2022-11-26 16:39:00 -0500

    don't use tab for nvim cmp keymaps

--- lua/plugin-configs/nvim-cmp.lua
+++ lua/plugin-configs/nvim-cmp.lua
@@ -21,10 +21,10 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = { i = cmp.mapping.select_next_item({}) },
     ['<C-f>'] = { i = cmp.mapping.scroll_docs(4) },
-    ['<C-Space>'] = { i = cmp.mapping.confirm({}) },
+    ['<C-k>'] = { i = cmp.mapping.confirm({ select = true }) },
     ['<C-e>'] = { i = cmp.mapping.abort() },
     ['<CR>'] = { i = cmp.mapping.confirm({ select = true }) }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-n>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -35,7 +35,7 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<C-p>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
