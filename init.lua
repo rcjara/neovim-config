887170d279cac1bc5285c0ee33afc92007c52a16
Raul Jara <raul.c.jara@gmail.com>
2023-09-29 12:06:15 -0400

    coq keybindings

--- init.lua
+++ init.lua
@@ -127,5 +127,17 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end
 })
 
+local coq_key_bindings = vim.api.nvim_create_augroup('coq_key_bindings', { clear = true })
+vim.api.nvim_create_autocmd('BufEnter', {
+  group =  coq_key_bindings,
+  pattern = '*.v',
+  callback = function (_bufnr)
+    nmap('<C-space>', ':CoqNext<CR>')
+    nmap('<C-p>', ':CoqPrev<CR>')
+    nmap('<space><space>', ':CoqToLine<CR>')
+  end
+})
+
+
 utils.setup_au_reload_command()
 utils.print_filename_on_reload()
